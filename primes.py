a7dc90c78a5d540cccc241429e4984ac8ecadc52
smilovanov <Stepan.Milovanov@infobip.com>
2022-07-29 23:05:14 +0300

    Added cosmetics and comments to code

--- primes.py
+++ primes.py
@@ -1,6 +1,6 @@
 import time
 
-
+#user input for calculations limit is packed in try to avoid typos and errors. Default is 10'000
 try:
 	calc_limit = int (input("Please enter limit for calculations: "))
 except:
@@ -8,12 +8,13 @@ except:
 finally: 
 	print (f"Limit set to {calc_limit}")
 
-#used only to measure optimization of script
-start_time = time.time ()
-
 #variable to format output
 format_len = len(str(calc_limit))
 
+#used only to measure optimization of script
+start_time = time.time ()
+
+#calculations block
 primes = {2}
 i = 3
 while i <= calc_limit:
@@ -22,9 +23,11 @@ while i <= calc_limit:
 		if i % divider == 0:
 			prime = False
 	if prime == True: 
-		print (f"{i:<{format_len}d}", end="")
+		print (f"{i:<{format_len}d}", end="",flush=True)
 		primes.add (i)
 	i += 1
 
-print (f"\n\n{len(primes)} prime numbers in total.")
+#summary block
+print (f"\n\nLimit was set to {calc_limit}")
+print (f"{len(primes)} prime numbers in total.")
 print (f"{time.time()-start_time:.3f} seconds spent on calculations.")
